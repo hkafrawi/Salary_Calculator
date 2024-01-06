fa24286a2800cd6671fc99b8081b4e88815f0cf9
hkafrawi <hassaballahelkafrawi@gmail.com>
2024-01-07 00:28:45 +0100

    Adjusted calc for larger salaries

--- salary_calc.py
+++ salary_calc.py
@@ -1,19 +1,25 @@
 tax_table_2023 = {0:[0,30_000],
-                  .10:[30_001,45_000],
-                  .15:[45_001,60_000],
-                  .20:[60_001,200_000],
-                  .225:[200_001,400_000],
-                  .25:400_001}
+                  .10:[30_000,45_000],
+                  .15:[45_000,60_000],
+                  .20:[60_000,200_000],
+                  .225:[200_000,400_000],
+                  .25:[400_000,]}
 
 def calc_tax(n):
     tax = 0
-    for x in tax_table_2023.keys():
-        if n > tax_table_2023[x][1]-tax_table_2023[x][0]:
-            tax = tax + (tax_table_2023[x]*tax_table_2023[x][1])
-            n = n - tax_table_2023[x][1]
+    keys = [key for key in tax_table_2023.keys()]
+    for x in keys[:-1]:
+        tax_level = tax_table_2023[x][1]-tax_table_2023[x][0]
+        if n > tax_level:
+            tax = tax + (x*tax_level)
+            n = n - tax_level
         else:
-            tax = tax + (tax_table_2023[x]*n)    
+            tax = tax + (x*n)
+            n = n - n    
             break
+    if n > 0:
+        tax = tax +(keys[-1]*n)
+        
         
     return tax
 
