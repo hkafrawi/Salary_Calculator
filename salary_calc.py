2509c38d36f6f8c8c5379c0e172649d0aea14eeb
hkafrawi <hassaballahelkafrawi@gmail.com>
2024-01-07 13:31:53 +0100

    Added Social Insurance Calc

--- salary_calc.py
+++ salary_calc.py
@@ -5,6 +5,9 @@ tax_table_2023 = {0:[0,30_000],
                   .225:[200_000,400_000],
                   .25:[400_000,]}
 
+social_insurance_frame_2023 = [1700,10900]
+social_insurance_frame_2000 = [1000,7000]
+
 def calc_tax(n):
     tax = 0
     keys = [key for key in tax_table_2023.keys()]
@@ -22,5 +25,27 @@ def calc_tax(n):
         
         
     return tax
+
+def calc_si(n, salary_frame:list, employee_owner_share= [0.11,0.1875]):
+    if (n>salary_frame[0]) & (n<=salary_frame[1]):
+        n = n*.80
+        employee_share = n*employee_owner_share[0]
+        owner_share = n*employee_owner_share[1]
+        
+    elif n<salary_frame[0]:
+        n=salary_frame[0]
+        n = n*.80
+        employee_share = n*employee_owner_share[0]
+        owner_share = n*employee_owner_share[1]
+        
+    elif n>salary_frame[1]:
+        n=salary_frame[1]
+        n = n*.80 
+        employee_share = n*employee_owner_share[0]
+        owner_share = n*employee_owner_share[1]
+        
+    return employee_share, owner_share
+
+
 
         
